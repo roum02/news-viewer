d23989c7c120864b5aed3acde846ba546dd99a11
roum02 <roum02@ewhain.net>
2021-10-19 22:44:56 +0900

    API

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import axios from "axios";
+
+// apiKey 3c3956ba55574481865e14506029d508
 
 function App() {
+  const [data, setData] = useState(null);
+  const onClick = async () => {
+    try {
+      const response = await axios.get(
+        "https://newsapi.org/v2/top-headlines?country=kr&apiKey=3c3956ba55574481865e14506029d508"
+      );
+      setData(response.data);
+    } catch (e) {
+      console.log(e);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <div>
+        <button onClick={onClick}>불러오기</button>
+      </div>
+      {data && (
+        <textarea
+          rows={7}
+          value={JSON.stringify(data, null, 2)}
+          readOnly={true}
+        />
+      )}
     </div>
   );
 }
